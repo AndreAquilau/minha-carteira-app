bda26a31186fed6dee2c5c6ad366d0f8987a2b12
AndreAquilau <andre.aquilau@outlook.com>
2021-01-21 00:47:20 -0400

    chore: add border-right aside

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -4,4 +4,5 @@ export const Container = styled.div`
   grid-area: AS;
   background-color: ${(props) => props.theme.colors?.secondary};
   color: ${(props) => props.theme.colors?.white};
+  border-right: 1px solid ${(props) => props.theme.colors?.gray};
 `;
